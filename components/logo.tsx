b6489f3bcf0224f64bc2ba372053263d1b99ed26
frantchessico <tchessico@live.com>
2023-09-08 13:39:43 +0200

    add some features

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,7 +10,13 @@ export default function Logo({ onClick }: LogoProps) {
       onClick={() => onClick()}
       className="cursor-pointer border max-w-fit px-2 py-1 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-secondary text-secondary-foreground hover:bg-secondary/80"
     >
+      <a href="https://github.com/frantchessico" target="_blank" style={{
+        listStyleType: 'none',
+        textDecoration: 'none'
+      }}>
       {logoText}
+      </a>
+      
     </div>
   );
 }
